bbf7001993decd99d03784988e5d075137917437
vinodkd <vinod@vinodkd.org>
2012-01-10 21:07:04 +0530

    added link to bash templating technique from SO

--- process_gv.sh
+++ process_gv.sh
@@ -1,3 +1,5 @@
 # process env vars in the gv and convert to display format
+# From http://stackoverflow.com/a/2915592
+
 sed -f replace.sed wiki_cs.gv > .wiki_cs_mod.gv
 dot -Tpng .wiki_cs_mod.gv -o wiki_cs.png
